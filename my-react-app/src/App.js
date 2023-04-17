25e87c3c3d0b76900a83d0b60ce6631851a16444
MORGANJASON <420morganjason@gmail.com>
2023-04-17 12:53:30 +0300

    created image and paragraph

---
+++ my-react-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://scontent.fmba5-1.fna.fbcdn.net/v/t1.6435-9/94122992_102520104780413_4661734567953039360_n.jpg?stp=c0.72.206.206a_dst-jpg_p206x206&_nc_cat=110&ccb=1-7&_nc_sid=da31f3&_nc_ohc=eo6JWHItmPsAX8nAlZY&_nc_ht=scontent.fmba5-1.fna&oh=00_AfBYnH4tX_MB3Vd20lnUGxiJRtJjfBb6-D1FeV69nWaFgg&oe=6464822B" className="Matatu" alt="logo" />
+        <p>
+          Morgan Jason an upcoming software developer!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
